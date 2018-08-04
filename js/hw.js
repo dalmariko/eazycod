9741e6562edcbd6b68aeca911845c5f888a19564
dalmariko <dalmarikocamp@gmail.com>
2018-08-04 21:25:26 +0300

    to 1.4

--- js/hw.js
+++ js/hw.js
@@ -4,3 +4,43 @@ let string = `
 В самом деле, почему
 Мёд так нравится ему?
 `;
+console.log(string);
+
+
+
+//1.1. Получить индекс второго вопросительного знака.
+// var Quest=string.indexOf('!')+1;
+// console.log(Quest);
+
+//1.2. Сделать второе слово «мед» с маленькой буквы.
+// var Med = string.indexOf('Мёд');
+// console.log(string.substr(Med,3).toLowerCase());
+
+// 1.3. Узнать включает ли строка слово «почему».
+// var Whay=string.indexOf('почему');
+// var word = Whay+6;
+// console.log(string.endsWith('почему',word));
+
+
+//1.4. Оканчивается ли строка на вопросительный знак.
+// console.log(string.endsWith('?\n'));
+
+
+
+
+//1.8. Получить символ, находящийся по индексу знака «!» увеличенному на 1.
+// var Wosc=string.indexOf('!')+1;
+// console.log(string[Wosc]);
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log();
