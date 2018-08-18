9e378431586e00e05da97e987c2dcda371c4f75b
dalmariko <dalmarikocamp@gmail.com>
2018-08-19 01:14:36 +0300

    all worck to do

--- js/hw.js
+++ js/hw.js
@@ -1,5 +1,43 @@
 //this is home worck aboute functions
 
-function myFunction() {
-    
+function reverse(value = '') {
+    if (!value) return console.log(`Please enter your text`);
+
+    let rez='';
+
+    for (let i = value.length-1; i >= 0; i--) {
+        rez += value[i];
+    }
+    return rez;
+}
+
+console.log(reverse('test'));
+
+function getUnicode(value = '') {
+    if (!value) return console.log(`Please enter your text`);
+
+    let rez ='';
+
+    for (let i=0; i < value.length; i++) {
+        rez += value.charCodeAt(i)+' ';
+    }
+
+    return rez;
+}
+
+console.log(getUnicode('hello'));
+
+
+let allTask =[];
+
+function addTask(value = '') {
+    if(!value) return console.log(`Please enter your tasck`);
+
+    const task = {
+        value:`${value}`,
+        id:`${allTask.length}`
+    };
+
+    allTask.push(task);
+    console.log(allTask);
 }
