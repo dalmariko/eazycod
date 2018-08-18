2c3a940a81fed8e8b496db632479be9800dfb9e1
dalmariko <dalmarikocamp@gmail.com>
2018-08-19 02:49:47 +0300

    all worck

--- js/hw.js
+++ js/hw.js
@@ -1,57 +1,69 @@
 //this is home worck aboute functions
 
-function reverse(value = '') {
-    if (!value) return console.log(`Please enter your text`);
+// function reverse(value = '') {
+//     if (!value) return console.log(`Please enter your text`);
+//
+//     let rez = '';
+//
+//     for (let i = value.length - 1; i >= 0; i--) {
+//         rez += value[i];
+//     }
+//     return rez;
+// }
+//
+// console.log(reverse('test'));
+//
+// function getUnicode(value = '') {
+//     if (!value) return console.log(`Please enter your text`);
+//
+//     let rez = '';
+//
+//     for (let i = 0; i < value.length; i++) {
+//         rez += value.charCodeAt(i) + ' ';
+//     }
+//
+//     return rez;
+// }
+//
+// console.log(getUnicode('hello'));
+//
 
-    let rez = '';
-
-    for (let i = value.length - 1; i >= 0; i--) {
-        rez += value[i];
-    }
-    return rez;
-}
-
-console.log(reverse('test'));
-
-function getUnicode(value = '') {
-    if (!value) return console.log(`Please enter your text`);
-
-    let rez = '';
-
-    for (let i = 0; i < value.length; i++) {
-        rez += value.charCodeAt(i) + ' ';
-    }
-
-    return rez;
-}
-
-console.log(getUnicode('hello'));
-
-
-let allTask = [];
+let allTask = [
+    {'task': "ferst tasck", 'id': "0"},
+    {'task': "next tasck", 'id': "1"},
+    {'task': "therd tasck", 'id': "2"}
+];
 
 
 function addTask(value = '') {
     if (!value) return console.log(`Please enter your tasck`);
 
     const task = {
-        value: `${value}`,
-        id: `${allTask.length}`
+        'task': `${value}`,
+        'id': `${allTask.length}`
     };
-
     allTask.push(task);
-    console.log(allTask);
 }
 
-function delTasck(id = '') {
-    if (!id) return console.log('Select tascks');
+function reId(somArr) {
 
-    id = parseInt(id, 10);
+    for (let i=0; i < somArr.length; i++){
+        somArr[i]['id'] = i + '';
+    }
+    return somArr;
+}
+
+function delTasck(id) {
+    if (id === '' ||  id === undefined) return console.log('Select tascks');
+
 
     for (let key of allTask) {
-        if (allTask[key][+id] === id) {
+        if (+(key['id']) === +id) {
             allTask.splice(key, 1);
             console.log('Tasck delete successfull');
+            reId(allTask);
         }
     }
 }
+
+console.log(allTask);
