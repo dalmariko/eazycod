156a4c60d24f03e02ab9e1b9bbb5d40d96837927
dalmariko <dalmarikocamp@gmail.com>
2018-08-19 13:09:47 +0300

    jshw4 worck

--- js/hw.js
+++ js/hw.js
@@ -1,62 +1,70 @@
-//this is home worck aboute functions
+(function () {
+function reverse(value = '') {
+    if (!value) return console.log(`Please enter your text`);
 
-// function reverse(value = '') {
-//     if (!value) return console.log(`Please enter your text`);
-//
-//     let rez = '';
-//
-//     for (let i = value.length - 1; i >= 0; i--) {
-//         rez += value[i];
-//     }
-//     return rez;
-// }
-//
-// console.log(reverse('test'));
-//
-// function getUnicode(value = '') {
-//     if (!value) return console.log(`Please enter your text`);
-//
-//     let rez = '';
-//
-//     for (let i = 0; i < value.length; i++) {
-//         rez += value.charCodeAt(i) + ' ';
-//     }
-//
-//     return rez;
-// }
-//
-// console.log(getUnicode('hello'));
-//
+    let rez = '';
+
+    for (let i = value.length - 1; i >= 0; i--) {
+        rez += value[i];
+    }
+    return rez;
+}
+
+alert(reverse('test'));
+
+function getUnicode(value = '') {
+    if (!value) return console.log(`Please enter your text`);
+
+    let rez = '';
+
+    for (let i = 0; i < value.length; i++) {
+        rez += value.charCodeAt(i) + ' ';
+    }
+
+    return rez;
+}
+
+alert(getUnicode('hello'));
+
+
+
+function randomDay(max = 32, min = 1) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+var d = new Date(2018, 7, 19);
+
+
+var timeBegin = d;
 
 let allTask = [
-    {'task': "0 tasck", 'id': 0},
-    {'task': "1 tasck", 'id': 1},
-    {'task': "2 tasck", 'id': 2},
-    {'task': "3 tasck", 'id': 3},
-    {'task': "4 tasck", 'id': 4},
-    {'task': "5 tasck", 'id': 5},
-    {'task': "6 tasck", 'id': 6},
-    {'task': "7 tasck", 'id': 7},
-    {'task': "8 tasck", 'id': 8},
-    {'task': "9 tasck", 'id': 9},
-    {'task': "10 tasck", 'id': 10},
-    {'task': "11 tasck", 'id': 11},
-    {'task': "12 tasck", 'id': 12},
-    {'task': "13 tasck", 'id': 13},
-    {'task': "14 tasck", 'id': 14},
-    {'task': "15 tasck", 'id': 15},
-    {'task': "16 tasck", 'id': 16},
-    {'task': "17 tasck", 'id': 17}
+    {
+        'task': "0 tasck",
+        'id': 0,
+        'timeBegin': timeBegin,
+        'timeFinish': new Date(d.getFullYear(), d.getMonth(), (d.getDay() + randomDay()))
+    },
+    {
+        'task': "1 tasck",
+        'id': 1,
+        'timeBegin': timeBegin,
+        'timeFinish': new Date(d.getFullYear(), d.getMonth(), (d.getDay() + randomDay()))
+    },
+    {
+        'task': "2 tasck",
+        'id': 2,
+        'timeBegin': timeBegin,
+        'timeFinish': new Date(d.getFullYear(), d.getMonth(), (d.getDay() + randomDay()))
+    },
 ];
 
 
 function addTask(value = '') {
     if (!value) return console.log(`Please enter your tasck`);
-
     const task = {
         'task': `${value}`,
         'id': +allTask.length
     };
+
     allTask.push(task);
 }
 
@@ -81,18 +89,25 @@ function manyTaskDel(delTasks = []) {
     }
 }
 
+function viewTasks(allTasks = []) {
+    let rez = '';
+    for (let key of allTasks) {
+        rez += `
+     <section class="task">
+        <ul class="taskContent">
+            <li class="id"><span>${key['id']}</span></li>
+            <li class="exercise"><span>${key['task']}</span></li>
+            <li class="timeBegin"><span>${key['timeBegin']}</span></li>
+            <li class="timeFinish"><span>${key['timeFinish']}</span></li>
+        </ul>
+    </section>
+`;
+    }
+    return rez;
+}
 
+    let doc = document;
+    let container=doc.getElementsByClassName('container')[0];
+    container.innerHTML=viewTasks(allTask);
 
-
-var date1 = Date.now();
-
-// console.log(allTask);
-console.log('\n');
-console.log(manyTaskDel([1, 15, 10]));
-console.log('\n');
-// console.log(allTask);
-var date2 = Date.now();
-
-console.log('\n');
-
-console.log(date2, ' ', date1, ' ', (date2 - date1) / 1000);
+})();
