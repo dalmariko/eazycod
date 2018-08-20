52360c01d4fca1cd19e877aede92f9278141c21e
dalmariko <dalmarikocamp@gmail.com>
2018-08-21 01:00:21 +0300

    camel function todo

--- js/hw.js
+++ js/hw.js
@@ -1,3 +1,31 @@
-/**
- * Created by dalmariko on 04.08.18.
- */
+function manager(obgect, handler) {
+    for (let i in obgect) {
+        return `New value: ${ handler(obgect) }`;
+    }
+}
+
+let controlArray = ['camelcaseconcat', 'multiplication', 'nameandage', 'reversestring'];
+
+function camelcaseConcat(element) {
+    let rez = '';
+    for (let index of element) {
+        for (let i=0; i < index.length; i++) {
+            rez += i === 0 ? index[i].toUpperCase() : index[i];
+        }
+    }
+    return rez;
+}
+
+function multiplication() {
+
+}
+
+function nameAndage() {
+
+}
+
+function reverseString() {
+
+}
+
+console.log(manager(controlArray, camelcaseConcat));
