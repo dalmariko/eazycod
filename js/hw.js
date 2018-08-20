cc44249f7fbcc363632026aaefa2f006d68aaf69
dalmariko <dalmarikocamp@gmail.com>
2018-08-21 02:10:56 +0300

    all hw is do

--- js/hw.js
+++ js/hw.js
@@ -4,14 +4,46 @@ function manager(obgect, handler) {
     }
 }
 
-let numbers = [10,20,30];
+let numbers = [10, 20, 30];
 
-let controlArray = ['camelcaseconcat', 'multiplication', 'nameandage', 'reversestring'];
+let users = [
+    {
+        age: 25,
+        name: 'Dima'
+    },
+    {
+        age: 21,
+        name: 'Vasja'
+    }
+];
+
+let abs123 = ['123', 'abc'];
+
+let fullName = ['neo,', 'my', 'name', 'trinity'];
+
+let controlArray = [
+    {
+        'function': camelcaseConcat,
+        'array': fullName
+    },
+    {
+        'function': multiplication,
+        'array': numbers
+    },
+    {
+        'function': nameManage,
+        'array': users
+    },
+    {
+        'function': reverseString,
+        'array': abs123
+    }
+];
 
 function camelcaseConcat(element) {
     let rez = '';
     for (let index of element) {
-        for (let i=0; i < index.length; i++) {
+        for (let i = 0; i < index.length; i++) {
             rez += i === 0 ? index[i].toUpperCase() : index[i];
         }
     }
@@ -19,20 +51,33 @@ function camelcaseConcat(element) {
 }
 
 function multiplication(element) {
-    let rez='';
-    for (let index of element){
-        rez += index*10+',';
+    let rez = '';
+    for (let index of element) {
+        rez += index * 10 + ',';
     }
     return rez;
 }
 
-function nameAndage() {
-
+function nameManage(elements) {
+    let rez = '';
+    for (let element of elements) {
+        rez += `${element['name']} is ${element['age']},`;
+    }
+    return rez;
 }
 
-function reverseString() {
-
+function reverseString(element) {
+    let rez = '';
+    for (let index of element) {
+        for (let i = index.length - 1; i >= 0; i--) {
+            rez += index[i];
+        }
+        rez += ',';
+    }
+    return rez;
 }
 
-console.log(manager(controlArray, camelcaseConcat));
-console.log(manager(numbers, multiplication));
+
+for (let callbecs of controlArray) {
+        console.log( manager(callbecs['array'],callbecs['function']) );
+}
