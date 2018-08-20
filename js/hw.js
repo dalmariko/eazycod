92f9c93da93fd1be45971d5f59bf57929dbb2177
dalmariko <dalmarikocamp@gmail.com>
2018-08-21 02:39:50 +0300

    autorun function

--- js/hw.js
+++ js/hw.js
@@ -1,83 +1,86 @@
-function manager(obgect, handler) {
-    for (let i in obgect) {
-        return `New value: ${ handler(obgect) }`;
-    }
-}
-
-let numbers = [10, 20, 30];
-
-let users = [
-    {
-        age: 25,
-        name: 'Dima'
-    },
-    {
-        age: 21,
-        name: 'Vasja'
-    }
-];
-
-let abs123 = ['123', 'abc'];
-
-let fullName = ['neo,', 'my', 'name', 'trinity'];
-
-let controlArray = [
-    {
-        'function': camelcaseConcat,
-        'array': fullName
-    },
-    {
-        'function': multiplication,
-        'array': numbers
-    },
-    {
-        'function': nameManage,
-        'array': users
-    },
-    {
-        'function': reverseString,
-        'array': abs123
-    }
-];
-
-function camelcaseConcat(element) {
-    let rez = '';
-    for (let index of element) {
-        for (let i = 0; i < index.length; i++) {
-            rez += i === 0 ? index[i].toUpperCase() : index[i];
+(function () {
+    function manager(obgect, handler) {
+        for (let i in obgect) {
+            return `New value: ${ handler(obgect) }`;
         }
     }
-    return rez;
-}
 
-function multiplication(element) {
-    let rez = '';
-    for (let index of element) {
-        rez += index * 10 + ',';
-    }
-    return rez;
-}
+    let numbers = [10, 20, 30];
 
-function nameManage(elements) {
-    let rez = '';
-    for (let element of elements) {
-        rez += `${element['name']} is ${element['age']},`;
-    }
-    return rez;
-}
-
-function reverseString(element) {
-    let rez = '';
-    for (let index of element) {
-        for (let i = index.length - 1; i >= 0; i--) {
-            rez += index[i];
+    let users = [
+        {
+            age: 25,
+            name: 'Dima'
+        },
+        {
+            age: 21,
+            name: 'Vasja'
         }
-        rez += ',';
+    ];
+
+    let abs123 = ['123', 'abc'];
+
+    let fullName = ['neo,', 'my', 'name', 'trinity'];
+
+    let controlArray = [
+        {
+            'function': camelcaseConcat,
+            'array': fullName
+        },
+        {
+            'function': multiplication,
+            'array': numbers
+        },
+        {
+            'function': nameManage,
+            'array': users
+        },
+        {
+            'function': reverseString,
+            'array': abs123
+        }
+    ];
+
+    function camelcaseConcat(element) {
+        let rez = '';
+        for (let index of element) {
+            for (let i = 0; i < index.length; i++) {
+                rez += i === 0 ? index[i].toUpperCase() : index[i];
+            }
+        }
+        return rez;
     }
-    return rez;
-}
+
+    function multiplication(element) {
+        let rez = '';
+        for (let index of element) {
+            rez += index * 10 + ',';
+        }
+        return rez;
+    }
+
+    function nameManage(elements) {
+        let rez = '';
+        for (let element of elements) {
+            rez += `${element['name']} is ${element['age']},`;
+        }
+        return rez;
+    }
+
+    function reverseString(element) {
+        let rez = '';
+        for (let index of element) {
+            for (let i = index.length - 1; i >= 0; i--) {
+                rez += index[i];
+            }
+            rez += ',';
+        }
+        return rez;
+    }
 
 
-for (let callbecs of controlArray) {
+    for (let callbecs of controlArray) {
         console.log( manager(callbecs['array'],callbecs['function']) );
-}
+    }
+
+})();
