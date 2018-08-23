25b052452b38392c28bf644a1191811551a4fd86
Dimka <manager@perilman.com.ua>
2018-08-23 18:03:35 +0300

    worck for dop tasck 2

--- js/hw.js
+++ js/hw.js
@@ -1,86 +1,115 @@
+// (function () {
+//     function manager(obgect, handler) {
+//         for (let i in obgect) {
+//             return `New value: ${ handler(obgect) }`;
+//         }
+//     }
+//
+//     let numbers = [10, 20, 30];
+//
+//     let users = [
+//         {
+//             age: 25,
+//             name: 'Dima'
+//         },
+//         {
+//             age: 21,
+//             name: 'Vasja'
+//         }
+//     ];
+//
+//     let abs123 = ['123', 'abc'];
+//
+//     let fullName = ['neo,', 'my', 'name', 'trinity'];
+//
+//     let controlArray = [
+//         {
+//             'function': camelcaseConcat,
+//             'array': fullName
+//         },
+//         {
+//             'function': multiplication,
+//             'array': numbers
+//         },
+//         {
+//             'function': nameManage,
+//             'array': users
+//         },
+//         {
+//             'function': reverseString,
+//             'array': abs123
+//         }
+//     ];
+//
+//     function camelcaseConcat(element) {
+//         let rez = '';
+//         for (let index of element) {
+//             for (let i = 0; i < index.length; i++) {
+//                 rez += i === 0 ? index[i].toUpperCase() : index[i];
+//             }
+//         }
+//         return rez;
+//     }
+//
+//     function multiplication(element) {
+//         let rez = '';
+//         for (let index of element) {
+//             rez += index * 10 + ',';
+//         }
+//         return rez;
+//     }
+//
+//     function nameManage(elements) {
+//         let rez = '';
+//         for (let element of elements) {
+//             rez += `${element['name']} is ${element['age']},`;
+//         }
+//         return rez;
+//     }
+//
+//     function reverseString(element) {
+//         let rez = '';
+//         for (let index of element) {
+//             for (let i = index.length - 1; i >= 0; i--) {
+//                 rez += index[i];
+//             }
+//             rez += ',';
+//         }
+//         return rez;
+//     }
+//
+//
+//     for (let callbecs of controlArray) {
+//         console.log( manager(callbecs['array'],callbecs['function']) );
+//     }
+//
+// })();
+
 (function () {
-    function manager(obgect, handler) {
-        for (let i in obgect) {
-            return `New value: ${ handler(obgect) }`;
+
+    let arr = [1, 21, 31, 12, 21, 12, 1, 1, 1, 10];
+
+    function avrage(arr) {
+        let sum = 0;
+        for (let index of arr) {
+            sum += index;
         }
+        return sum / arr.length;
     }
 
-    let numbers = [10, 20, 30];
+    // console.log(avrage(arr));
 
-    let users = [
-        {
-            age: 25,
-            name: 'Dima'
-        },
-        {
-            age: 21,
-            name: 'Vasja'
-        }
-    ];
+    function delNum(arr, repiat) {
+        let rez = arr;
+            for (let i = 0; i < rez.length; i++) {
+                for(let x =0; x<arr.length;x++){
 
-    let abs123 = ['123', 'abc'];
-
-    let fullName = ['neo,', 'my', 'name', 'trinity'];
-
-    let controlArray = [
-        {
-            'function': camelcaseConcat,
-            'array': fullName
-        },
-        {
-            'function': multiplication,
-            'array': numbers
-        },
-        {
-            'function': nameManage,
-            'array': users
-        },
-        {
-            'function': reverseString,
-            'array': abs123
-        }
-    ];
-
-    function camelcaseConcat(element) {
-        let rez = '';
-        for (let index of element) {
-            for (let i = 0; i < index.length; i++) {
-                rez += i === 0 ? index[i].toUpperCase() : index[i];
-            }
+                }
         }
         return rez;
     }
 
-    function multiplication(element) {
-        let rez = '';
-        for (let index of element) {
-            rez += index * 10 + ',';
-        }
-        return rez;
-    }
+    console.log(delNum(arr, 2));
 
-    function nameManage(elements) {
-        let rez = '';
-        for (let element of elements) {
-            rez += `${element['name']} is ${element['age']},`;
-        }
-        return rez;
-    }
-
-    function reverseString(element) {
-        let rez = '';
-        for (let index of element) {
-            for (let i = index.length - 1; i >= 0; i--) {
-                rez += index[i];
-            }
-            rez += ',';
-        }
-        return rez;
-    }
-
-
-    for (let callbecs of controlArray) {
-        console.log( manager(callbecs['array'],callbecs['function']) );
-    }
 
 })();
