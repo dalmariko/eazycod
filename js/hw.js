478e10cbb0efbd3e7c9048d5bf7e8961ff0e8d87
Dimka <manager@perilman.com.ua>
2018-09-02 19:02:18 +0300

    ferst part

---
+++ js/hw.js
@@ -0,0 +1,36 @@
+/*
+* Зная структуру HTML
+* с помощью изученных методов получить в консоль
+*
+* 1 head
+* 2 body
+* 3 все дочерние элементы body
+* 4 первый div и все его дочерние узлы
+*   а)вывести все дочерние узлы в консоль
+*   б)вывести в консоль все дочерние узлы,
+*  кроме первого и последнего.
+*
+*
+* Для навигациипо ДОМу использовать методы,
+* которые возвращают только элементы.
+*
+* */
+
+
+const head=document.head;
+// console.log(head);
+
+const body=document.body;
+// console.log(body);
+
+const children=body.children;
+// console.log(children);
+
+let [... allElements]=children;
+// console.log(allElements);
+
+console.log(typeof allElements);
+
+// let div=allElements.forEach( (div) => 0 );
+//
+// console.log(div);
