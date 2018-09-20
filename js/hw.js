38eb0154c03af1f6514e50a6b98fa77e849492bd
dalmariko <dalmarikocamp@gmail.com>
2018-09-20 14:53:37 +0300

    2 tasck do

--- js/hw.js
+++ js/hw.js
@@ -1,14 +1,25 @@
-const getSquare = (width, height) => {
-    this.getSquare = width * height;
-    return this;
-};
-
 const rectangle = {
     width: 1,
     height: 1,
-    getSquare: getSquare
+    getSquare: function ()  {
+        return this.width * this.height;
+    }
 };
 
-console.log(rectangle.getSquare(2, 4));
+// console.log(rectangle.getSquare(rectangle.width=5,rectangle.height=4));
+
+
+const price = {
+    price: 10,
+    discount: '15%',
+    getPrice: function () {
+        return this.price;
+    },
+    getPriceWithDiscount: function () {
+        return this.price - (this.price * (parseFloat(this.discount) / 100));
+    }
+};
+
+// console.log(price.getPriceWithDiscount(), price.getPrice());
 
 
