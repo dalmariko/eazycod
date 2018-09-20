9329d154c106272f7e8fc555d514241bebf9394a
dalmariko <dalmarikocamp@gmail.com>
2018-09-20 19:39:45 +0300

    five tasck do

--- js/hw.js
+++ js/hw.js
@@ -40,3 +40,21 @@ const heigth={
 
 // console.log(heigth.up());
 
+const numerator = {
+    value:1,
+    double: function () {
+        this.value*=2;
+        return this;
+    },
+    plusOne: function () {
+        this.value+=1;
+        return this;
+    },
+    minusOne: function () {
+        this.value-=1;
+        return this;
+    }
+};
+
+// console.log(numerator.double().plusOne().plusOne().minusOne());
+// console.log(numerator.value);
