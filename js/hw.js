1e142244f8acc7d6a11f8be29b172c66a77a9f20
dalmariko <dalmarikocamp@gmail.com>
2018-09-25 09:57:41 +0300

    Car to do

--- js/hw.js
+++ js/hw.js
@@ -1,13 +1,28 @@
+/**
+ * Констроуктор для машин
+ * @param name
+ * @param age
+ * @constructor
+ *
+ * @method getName {sring} return Name
+ * @method getBerdYear {sring} return berdth year car
+ *
+ */
+
 function Car(name,age) {
 this.name=name;
 this.age=age;
 
     this.getName=function () {
-        return this.name;
+         return this.name.slice(0,1).toUpperCase()+this.name.slice(1);
     };
 
     this.getBerdYear = function () {
-        let Year = Date.now();
-        return Year.Date
+        let Year =new Date().getFullYear();
+        return Year-this.age;
     }
 }
+
+let lexus = new Car('lexus',2);
+console.log(lexus.getName());
+console.log(lexus.getBerdYear());
