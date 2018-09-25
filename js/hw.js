63fa689b6c41df386fda05ed4f8e7b95595e424f
dalmariko <dalmarikocamp@gmail.com>
2018-09-25 09:27:48 +0300

    construct

---
+++ js/hw.js
@@ -0,0 +1,13 @@
+function Car(name,age) {
+this.name=name;
+this.age=age;
+
+    this.getName=function () {
+        return this.name;
+    };
+
+    this.getBerdYear = function () {
+        let Year = Date.now();
+        return Year.Date
+    }
+}
