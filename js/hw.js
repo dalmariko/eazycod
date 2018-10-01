e1d059517cb7a971b2dd3c33f38b11ce6aaf3d61
dalmariko <dalmarikocamp@gmail.com>
2018-10-01 12:51:39 +0300

    tasck do

--- js/hw.js
+++ js/hw.js
@@ -15,7 +15,7 @@ function Building(name,floors) {
 
 const somBuild =new Building('home',5);
 const setfloors = somBuild.setFloors(100);
-console.log(setfloors);
+// console.log(setfloors);
 
 function Home(name,floors,apartaments) {
     Building.apply(this,arguments);
@@ -32,7 +32,7 @@ function Home(name,floors,apartaments) {
 }
 
 const myHome = new Home('asd',5,32);
-console.log(myHome.getFloors());
+// console.log(myHome.getFloors());
 
 
 function Mart(name,floors,malls) {
@@ -50,5 +50,46 @@ function Mart(name,floors,malls) {
 }
 
 const myMart= new Mart('asd',3,50);
-console.log(myMart.getFloors());
+// console.log(myMart.getFloors());
+
+
+
+function User(name,regDate) {
+    this.name=name;
+    this.regDate=regDate;
+}
+
+User.prototype.getInfo=function () {
+    return this;
+};
+
+function Admin(name,registration,superadmin) {
+    User.apply(this,arguments);
+    this._superAdmin = superadmin;
+}
+
+function Guest(name,registration,superadmin,valiDate) {
+    User.apply(this,arguments);
+    this._valiDate = valiDate;
+}
+
+
+Admin.prototype = Object.create(User.prototype);
+Admin.prototype.constructor=Admin;
+
+Guest.prototype=Object.create(User.prototype);
+Guest.prototype.constructor=Guest;
+
+
+
+
+const user = new User('kate','11.09.1990');
+console.log(user.getInfo());
+
+const admin = new Admin('vasya','12.23.1933',false);
+console.log(admin.getInfo());
+
+const guest = new Guest('anna','16.11.99',false,'lastWeack');
+console.log(guest.getInfo());
+console.log(guest.name);
 
