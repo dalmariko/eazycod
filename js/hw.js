08eca8133a4ebf69059630bc406b707eec32dda0
dalmariko <dalmarikocamp@gmail.com>
2018-10-01 13:15:13 +0300

    all do

--- js/hw.js
+++ js/hw.js
@@ -1,95 +1,113 @@
-function Building(name,floors) {
-    this.name=name;
-    this.floors=floors;
+function Building(name, floors) {
+    this.name = name;
+    this.floors = floors;
 
-    this.getFloors=function () {
+    this.getFloors = function () {
         return this.floors;
     };
 
-    this.setFloors=function (floors) {
-        return this.floors=floors;
+    this.setFloors = function (floors) {
+        return this.floors = floors;
     };
 
 }
 
 
-const somBuild =new Building('home',5);
+const somBuild = new Building('home', 5);
 const setfloors = somBuild.setFloors(100);
-// console.log(setfloors);
+console.log(setfloors);
 
-function Home(name,floors,apartaments) {
-    Building.apply(this,arguments);
+function Home(name, floors, apartaments) {
+    Building.apply(this, arguments);
     this.apartaments = apartaments;
 
-    const getFloors=this.getFloors;
-            this.getFloors = function () {
-                getFloors.call(this);
-                return{
-                    'этажи':this.floors,
-                    'всего квартир':this.floors*this.apartaments
-                }
-            }
+    const getFloors = this.getFloors;
+    this.getFloors = function () {
+        getFloors.call(this);
+        return {
+            'этажи': this.floors,
+            'всего квартир': this.floors * this.apartaments
+        }
+    }
 }
 
-const myHome = new Home('asd',5,32);
-// console.log(myHome.getFloors());
+const myHome = new Home('asd', 5, 32);
+console.log(myHome.getFloors());
 
 
-function Mart(name,floors,malls) {
-    this.malls=malls;
-    Home.apply(this,arguments);
+function Mart(name, floors, malls) {
+    this.malls = malls;
+    Home.apply(this, arguments);
 
     const getFloors = this.getFloors;
-        this.getFloors = function () {
-            getFloors.call(this);
-            return {
-                'этажи':this.floors,
-                'всего магазинов':this.floors*this.malls
-            }
+    this.getFloors = function () {
+        getFloors.call(this);
+        return {
+            'этажи': this.floors,
+            'всего магазинов': this.floors * this.malls
         }
+    }
 }
 
-const myMart= new Mart('asd',3,50);
-// console.log(myMart.getFloors());
+const myMart = new Mart('asd', 3, 50);
+console.log(myMart.getFloors());
 
 
-
-function User(name,regDate) {
-    this.name=name;
-    this.regDate=regDate;
+function User(name, registration) {
+    this.name = name;
+    this.registration = registration;
 }
 
-User.prototype.getInfo=function () {
-    return this;
+User.prototype.getInfo = function () {
+    return {
+        'name': this.name,
+        'registration': this.registration
+    }
 };
 
-function Admin(name,registration,superadmin) {
-    User.apply(this,arguments);
-    this._superAdmin = superadmin;
+function Admin(name, registration, superadmin) {
+    User.apply(this, arguments);
+    // this._superAdmin = superadmin;
+    Admin.prototype.getInfo = function () {
+        User.prototype.getInfo(this);
+        return {
+            'name': this.name,
+            'registration': this.registration,
+            'superadmin': superadmin
+        }
+    }
+
 }
 
-function Guest(name,registration,superadmin,valiDate) {
-    User.apply(this,arguments);
-    this._valiDate = valiDate;
+
+function Guest(name, registration, superadmin, validate) {
+    User.apply(this, arguments);
+    Guest.prototype.getInfo = function () {
+        User.prototype.getInfo(this);
+        return {
+            'name': this.name,
+            'registration': this.registration,
+            'superadmin': superadmin,
+            'validate': validate
+        }
+    }
 }
 
 
 Admin.prototype = Object.create(User.prototype);
-Admin.prototype.constructor=Admin;
+Admin.prototype.constructor = Admin;
 
-Guest.prototype=Object.create(User.prototype);
-Guest.prototype.constructor=Guest;
-
-
+Guest.prototype = Object.create(User.prototype);
+Guest.prototype.constructor = Guest;
 
 
-const user = new User('kate','11.09.1990');
+const user = new User('kate', '11.09.1990');
 console.log(user.getInfo());
 
-const admin = new Admin('vasya','12.23.1933',false);
+const admin = new Admin('vasya', '12.23.1933', false);
 console.log(admin.getInfo());
 
-const guest = new Guest('anna','16.11.99',false,'lastWeack');
+const guest = new Guest('anna', '16.11.99', false, 'lastWeack');
 console.log(guest.getInfo());
-console.log(guest.name);
+console.log(guest.validate);
 
