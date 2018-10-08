c5fb6215b52834aa595e013812f57e402e5e52e6
dalmariko <dalmarikocamp@gmail.com>
2018-10-08 17:55:47 +0300

    parse object

---
+++ js/hw.js
@@ -0,0 +1,146 @@
+/**
+ * AJAX (Asynchronous JavaScript And Xml);
+ * GET  - получить информацию
+ * POST - передать инфу на сервер
+ * PUT - изменить сущкествующие данные
+ * DELETE - удалить данные на сервере
+ *
+ * JSON
+ */
+/*
+
+ const api_url = "https://jsonplaceholder.typicode.com";
+
+ const xhr = new XMLHttpRequest();
+
+ //  Настройка
+ xhr.open("POST", `${api_url}/posts`);
+ xhr.setRequestHeader("Content-type", "application/json");
+ // отправка запроса
+
+ const post = {
+ title: "New title",
+ body: "Some text",
+ userId: 1
+ };
+
+ xhr.send(JSON.stringify(post));
+ // console.log(xhr.responseText, new Date());
+
+ xhr.addEventListener("load", () => {
+ console.log(JSON.parse(xhr.responseText), new Date());
+ });
+
+ xhr.addEventListener("error", () => {
+ console.log(xhr.statusText);
+ });
+ */
+
+
+// TODO: Сделать запрос на сервер по адресу "https://jsonplaceholder.typicode.com/users"
+
+// TODO: Получить ответ преобразовать его из json в обычный массив объектов
+
+// TODO: вывести на страницу список пользователей, выводить только имя (name)
+
+// TODO: при клике на имя пользователя у меня должен открыться блок с подробной информацией об этом пользователе
+
+// const url = 'https://jsonplaceholder.typicode.com';
+// const xhr = new XMLHttpRequest();
+// xhr.open('get', `${url}/users`);
+// xhr.setRequestHeader("Content-type", "application/json");
+// xhr.send();
+// xhr.addEventListener('load', () => {
+//     const getAllUsers = document.querySelector('.users');
+//
+//     const users = JSON.parse(xhr.responseText);
+//     console.log(users);
+//
+//
+//
+// });
+//
+//
+// xhr.addEventListener('error', () => {
+//     console.error(`Произошла ошибка соединения по адрессу ${url}`);
+// });
+
+/*
+
+ users.forEach((item)=>{
+ const usersInfo =
+ `
+ <h4>${item.name}</h4>
+ <h5>${item.email}</h5>
+ <h6>${item.phone}</h6>
+ <h7>${item.username}</h7>
+ </br>
+ </br>
+ <h8><a href="${item.website}">${item.website}</a></h8>
+ <hr>
+ `;
+
+ getAllUsers.insertAdjacentHTML('afterbegin', usersInfo);
+ });
+ */
+
+
+const users = [{
+    email: "Sincere@april.biz",
+    id: 1,
+    name: "Leanne Graham",
+    phone: "1-770-736-8031 x56442",
+    username: "Bret",
+    website: "hildegard.org",
+    address: {
+        city: "Gwenborough",
+        street: "Kulas Light",
+        suite: "Apt. 556",
+        zipcode: "92998-3874",
+        geo: {
+            lat: "-37.3159",
+            lng: "81.1496"
+        }
+    },
+    comopany: {
+        bs: "harness real-time e-markets",
+        catchPhrase: "Multi-layered client-server neural-net",
+        name: "Romaguera-Crona",
+    }
+}
+];
+
+
+function recursivUserInfo(user) {
+    let prop = Object.keys(user);
+    let show;
+    for (let key of prop) {
+        if (typeof  user[key] === 'object') {
+            recursivUserInfo(user[key]);
+        }else{
+            show = `${key}:${user[key]}\n`;
+        }
+        console.log(show);
+    }
+    return show;
+}
+
+users.forEach(recursivUserInfo);
+
+/*
+ users.forEach((item)=>{
+ console.log(Object.keys(item));
+ });*/
+
+/*
+ let show;
+ Object.keys(user).forEach((key)=>{
+ // console.log(typeof user[key]);
+
+ if (user[key].length > 0){
+ recursivUserInfo(user[key]);
+ }
+ show =`${key} : ${user[key]}`;
+ console.log(show);
+ });
+ return show;*/
